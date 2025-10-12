43d3a113bc6453fe585e0ebd96b4988a73794fe8
Fatima <fatimakhm@code.edu.az>
2025-10-12 09:40:30 +0400

    Add files via upload

---
+++ tasks/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //int[] array = { 1, 2, 3, 9 };
+            //int k = 9;
+            //int number = -1;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    if (array[i] == k)
+            //    {
+            //        number = i; break;
+            //    }
+
+            //}
+            //Console.WriteLine(number); 
+            #endregion
+
+            #region Task2
+
+            //int[] nums = { 2, 2, 3, 4, 4, 6, 4, 8, 9 };
+            //int num = 2;
+            //int count = 0;
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (nums[i] == num)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+            #endregion
+
+            #region Task3
+            //string word = "salam";
+            //int max = 0;
+            //char simvol = ' ';
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    int count = 0;
+            //    for (int j = 0; j < word.Length; j++)
+            //    {
+            //        if (word[i] == word[j])
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    if (count > max)
+            //    {
+            //        max = count;
+            //        simvol = word[i];
+            //    }
+
+            //}
+            //Console.WriteLine(simvol);
+            //Console.WriteLine(max); 
+            #endregion
+            
+        }
+    }
+}
